59a901d4dfdb04bc7d20e34b5b906ab3b0f35803
nevadasurfer <jason@kennedyit.com>
2024-12-14 19:09:47 -0700

    clean up and setup

--- README.md
+++
@@ -1,3 +0,0 @@
-# localdev1
-# localdev1
-# localdev1
---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "kind" {}
+
+locals {
+  k8s_config_path = pathexpand("~/.kube/config")
+}
+
+# creating a cluster with kind of the name "test-cluster" with kubernetes version v1.27.1 and two nodes
+resource "kind_cluster" "default" {
+  name            = "test-cluster"
+  kubeconfig_path = local.k8s_config_path
+  node_image      = "kindest/node:v1.31.2"
+  kind_config {
+    kind        = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+    node {
+      role = "control-plane"
+    }
+    node {
+      role = "worker1"
+    }
+    node {
+      role = "worker2"
+    }
+  }
+}
