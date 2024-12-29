4da2e6c239f9a63a36b10f86eb591ec78d6ceb19
nevadasurfer <jason@kennedyit.com>
2024-12-29 14:15:45 -0700

    multipule providers

--- main.tf
+++ main.tf
@@ -1,41 +1,31 @@
+# main.tf
 terraform {
   required_providers {
+    kind = {
+      source  = "tehcyx/kind"
+      version = "~> 0.3.0"
+    }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.9.0"
+      version = "~> 2.8.0"
     }
   }
+
+  required_version = ">= 1.3.0"
 }
 
-#provider "helm" {
-#  kubernetes {
-#    config_path = "~/.kube/config" # Path to your Kubernetes config file
-#  }
-#}
-
+# Provider for kind
 provider "kind" {}
-locals {
-  k8s_config_path = pathexpand("~/.kube/config")
+
+# Create a Kubernetes cluster using kind
+resource "kind_cluster" "k8s" {
+  name = "localdev1"
 }
 
-# creating a cluster with kind of the name "test-cluster" with kubernetes version v1.27.1 and two nodes
-resource "kind_cluster" "default" {
-  name            = "test-cluster"
-  kubeconfig_path = local.k8s_config_path
-  node_image      = "kindest/node:v1.31.2"
-  kind_config {
-    kind        = "Cluster"
-    api_version = "kind.x-k8s.io/v1alpha4"
-    node {
-      role = "control-plane"
-    }
-    node {
-      role = "worker1"
-    }
-    node {
-      role = "worker2"
-    }
+# Provider for helm, connected to the kind cluster
+provider "helm" {
+  kubernetes {
+    config_path = kind_cluster.k8s.kubeconfig
   }
 }
-
 
