00987d9247814e23616a1a7053cd06c152dafb07
nevadasurfer <jason@kennedyit.com>
2024-12-30 15:44:35 -0700

    working with argo

---
+++ argocd.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "argocd" {
+  depends_on       = [kind_cluster.k8s]
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "3.35.4"
+  values           = [file("values.yaml")]
+}
+
