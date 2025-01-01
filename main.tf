24f395526b924d6973df3d85dff6eba12ef0077d
nevadasurfer <jason@kennedyit.com>
2024-12-31 18:50:10 -0700

    adding extra nodes

--- main.tf
+++ main.tf
@@ -14,19 +14,35 @@ terraform {
   required_version = ">= 1.3.0"
 }
 
-# Provider for kind
-provider "kind" {}
-
-# Define the kind cluster with 2 worker nodes
 resource "kind_cluster" "k8s" {
-  name = "my-cluster"
+  name           = "localdev1"
+  node_image     = "kindest/node:v1.32.0"
+  wait_for_ready = true
 
+  kind_config {
+    kind        = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+
+    node {
+      role = "control-plane"
+      extra_port_mappings {
+        container_port = 80
+        host_port      = 80
+      }
+    }
+
+    node {
+      role = "worker1"
+    }
+    node {
+      role = "worker2"
+    }
+  }
 }
 
 # Provider for helm, connected to the kind cluster
 provider "helm" {
   kubernetes {
-    #config_path = kind_cluster.k8s.kubeconfig
     config_path = "~/.kube/config"
   }
 }
